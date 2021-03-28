6a649dfd8811df5915d58928babad4a192c4d839
Zach <venanziz@gmail.com>
2021-03-28 11:43:13 -0400

    First Commit

--- BoatSchool/ViewController.swift
+++ BoatSchool/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabl: UILabel!
+    @IBOutlet weak var answerOne: UILabel!
+    @IBOutlet weak var answerTwo: UILabel!
+    @IBOutlet weak var answerThree: UILabel!
+    @IBOutlet weak var answerFour: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var data = readDataFromCSV(fileName: FILE_1100, fileType: kCSVFileExtension)
+        data = cleanRows(file: data!)
+        let csvRows = csv(data: data!)
+
         // Do any additional setup after loading the view.
     }
-
-
+    func csv(data: String) -> [[String]] {
+        var result: [[String]] = []
+        let rows = data.components(separatedBy: "\n")
+        for row in rows {
+            let columns = row.components(separatedBy: ";")
+        }
+        return result
+    }
+    var data = readDataFromCSV(fileName: kCSVFileName)
 }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+
+target 'BoatSchool' do
+  use_frameworks!
+pod 'CoreXLSX'
+
+
+  target 'BoatSchoolTests' do
+    inherit! :search_paths
+  end
+
+  target 'BoatSchoolUITests' do
+  end
+
+end
