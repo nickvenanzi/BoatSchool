0ebc5333496bcb46d5aa094fa5aca10a8cde6d7e
Zach <venanziz@gmail.com>
2021-04-09 20:29:11 -0400

    I failed to merge

--- BoatSchool/Cells/SectionHeader.swift
+++ BoatSchool/Cells/SectionHeader.swift
@@ -17,21 +17,21 @@ class SectionHeader: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
         
-        let margins = self.layoutMarginsGuide
-         
-        // Pin the leading edge of myView to the margin's leading edge
-        questionLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
-        questionLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
-        
-        questionImage.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
-        questionImage.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
-         
-        questionLabel.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
-        questionLabel.bottomAnchor.constraint(equalTo: questionImage.topAnchor).isActive = true
-        
-        questionImage.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
-        
-        questionImage.contentMode = .scaleAspectFit
+//        let margins = self.layoutMarginsGuide
+//         
+//        // Pin the leading edge of myView to the margin's leading edge
+//        questionLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+//        questionLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+//        
+//        questionImage.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+//        questionImage.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+//         
+//        questionLabel.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+//        questionLabel.bottomAnchor.constraint(equalTo: questionImage.topAnchor).isActive = true
+//        
+//        questionImage.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+//        
+//        questionImage.contentMode = .scaleAspectFit
 
     }
     
